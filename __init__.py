6c3bf902428f572adc85040aeee0b2596acc12e9
qyy <qyycoe@pku.edu.cn>
2026-01-21 21:09:43 +0800

    delete useless files

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2024
-# Alice Isaac - Humanoid Quadrotor IsaacLab Project
-
-"""Humanoid Quadrotor Project for IsaacLab."""
-
-__version__ = "0.1.0"
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Quick test script for humanoid quadrotor environment
-
-cd "$(dirname "$0")"
-
-echo "Testing Humanoid Quadrotor Environment..."
-python scripts/test_env.py --num_envs 4 --headless
